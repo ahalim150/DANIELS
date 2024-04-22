160d4782232409d9c15c91fef912b024ce6f2880
Ahmed Abd El-Halim <a.halim15@hotmail.com>
2024-04-22 22:21:47 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ["I'm Halim, Web Developer", "I'm Halim, Web Designer"],
+    typeSpeed: 50,
+    startDelay: 500,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+  });
